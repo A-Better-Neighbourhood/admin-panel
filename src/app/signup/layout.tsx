ade5844152bcad50f2edeade2dafdcb9173d3866
LaveshG11 <lavesh.23b0121010@abes.ac.in>
2025-11-28 12:18:45 +0530

    re initiated

--- src/app/signup/layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-// This layout removes header/footer by only rendering children
-export default function SignupLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
