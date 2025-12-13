234dbe22c1f125405f1a05967257ab97bb6c6bd3
Kunal Rana <kunalranarj2005@gmail.com>
2025-12-13 10:06:01 +0530

    stripped out content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
+/** @format */
+
 // import Image from "next/image";
 import React from "react";
-import { Login as LoginComponent } from "../components";
-
 
 export default function Home() {
-  return (
-    <div className="py-8">
-      <LoginComponent />
-    </div>
-  );
+  return <div className="py-8"></div>;
 }
